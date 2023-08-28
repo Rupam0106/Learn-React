5280292055ae1f11bf09ea46634ba227f63d880a
Rupam0106 <rupam.gupta@rudrainnovative.in>
2023-08-28 13:51:50 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button className="btn btn-primary">Submit</button>
+    </>
+  );
+}
+
+export default App;
